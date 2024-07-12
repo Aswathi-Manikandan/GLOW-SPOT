03e4a95e03729cdedbc275ea334965b1c8167cf0
ahwathy <aswathiachus935@gmail.com>
2024-07-11 17:31:55 -0700

    admin router protection

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,3 +1,4 @@
+// routes/adminRouter.js
 const express = require('express');
 const adminRoute = express();
 const session = require('express-session');
@@ -7,17 +8,16 @@ const path = require('path');
 const adminController = require('../controllers/adminController');
 const categoryController = require('../controllers/categoryController');
 const productController = require('../controllers/productController');
-const adminOrderController = require('../controllers/adminOrderController')
+const adminOrderController = require('../controllers/adminOrderController');
 const offerController = require('../controllers/offerController');
-const couponController = require('../controllers/couponController')
-const salesController = require('../controllers/salesController')
+const couponController = require('../controllers/couponController');
+const salesController = require('../controllers/salesController');
 
 const auth = require('../middleware/adminAuth');
 
 // Set view engine and views directory
 adminRoute.set('view engine', 'ejs');
 adminRoute.set('views', './views/admin');
-
 
 // Multer setup
 const storage = multer.diskStorage({
@@ -36,13 +36,13 @@ adminRoute.get('/', auth.isLogout, adminController.loadLogin);
 adminRoute.post('/', adminController.verifyLogin);
 adminRoute.get('/home', auth.isLogin, adminController.loadDashboard);
 adminRoute.get('/logout', auth.isLogin, adminController.logout);
-adminRoute.get('/home', adminController.userSearch);
+adminRoute.get('/home', auth.isLogin, adminController.userSearch);
 
-adminRoute.get('/usermanagement', adminController.adminDashboard);
-adminRoute.get('/users', adminController.getUsers);
-adminRoute.post('/users/:userId/block', adminController.blockUser);
-adminRoute.post('/users/:userId/unblock', adminController.unblockUser);
-adminRoute.post('/users/:userId/toggle-block', adminController.toggleUserBlock);
+adminRoute.get('/usermanagement', auth.isLogin, adminController.adminDashboard);
+adminRoute.get('/users', auth.isLogin, adminController.getUsers);
+adminRoute.post('/users/:userId/block', auth.isLogin, adminController.blockUser);
+adminRoute.post('/users/:userId/unblock', auth.isLogin, adminController.unblockUser);
+adminRoute.post('/users/:userId/toggle-block', auth.isLogin, adminController.toggleUserBlock);
 
 adminRoute.get('/category', auth.isLogin, categoryController.loadCategory);
 adminRoute.get('/category/add', auth.isLogin, categoryController.loadAddCategory);
@@ -60,34 +60,22 @@ adminRoute.post('/products/addpost', auth.isLogin, upload.array('images', 4), pr
 adminRoute.get('/products/edit/:id', auth.isLogin, productController.loadEditProduct);
 adminRoute.post('/products/edit/:id', auth.isLogin, upload.array('images', 4), productController.editProduct);
 adminRoute.get('/products/toggle/:id', auth.isLogin, productController.toggleProductStatus);
-adminRoute.post('/products/update-offer/:id', productController.updateProductOffer);
-
-
+adminRoute.post('/products/update-offer/:id', auth.isLogin, productController.updateProductOffer);
 
 adminRoute.post('/products/edit/:id/delete-image', auth.isLogin, productController.deleteImage);
 
+adminRoute.get('/orders', auth.isLogin, adminOrderController.listOrdersAdmin);
+adminRoute.post('/orders/update-order-status', auth.isLogin, adminOrderController.updateOrderStatus);
 
-
-adminRoute.get('/orders', adminOrderController.listOrdersAdmin);
-
-adminRoute.post('/orders/update-order-status', adminOrderController.updateOrderStatus);
-
-//-------------------offer-----------------------------------//
-
-
+// Offer routes
 adminRoute.get('/offers', auth.isLogin, offerController.loadOffersPage);
-
-// Add new offer
 adminRoute.get('/offers/add', auth.isLogin, offerController.loadAddOfferPage);
 adminRoute.post('/offers/add', auth.isLogin, offerController.addOffer);
-
-// Edit offer
 adminRoute.get('/offers/edit/:id', auth.isLogin, offerController.loadEditOfferPage);
 adminRoute.post('/offers/edit/:id', auth.isLogin, offerController.editOffer);
 adminRoute.delete('/offers/delete/:id', auth.isLogin, offerController.deleteOffer);
 
-
-
+// Coupon routes
 adminRoute.get('/coupons', auth.isLogin, couponController.loadCouponsPage);
 adminRoute.get('/coupons/add', auth.isLogin, couponController.loadAddCouponPage);
 adminRoute.post('/coupons/add', auth.isLogin, couponController.addCoupon);
@@ -95,10 +83,10 @@ adminRoute.get('/coupons/edit/:id', auth.isLogin, couponController.loadEditCoupo
 adminRoute.post('/coupons/edit/:id', auth.isLogin, couponController.editCoupon);
 adminRoute.delete('/coupons/delete/:id', auth.isLogin, couponController.deleteCoupon);
 
+// Sales routes
 adminRoute.get('/sales', auth.isLogin, salesController.loadSalesReport);
 adminRoute.get('/sales/pdf', auth.isLogin, salesController.generatePDFReport);
 adminRoute.get('/sales/excel', auth.isLogin, salesController.generateExcelReport);
-
 
 // Redirect all other routes to /admin
 adminRoute.get('*', (req, res) => {
